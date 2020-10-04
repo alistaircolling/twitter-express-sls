c9b33dcab61896d75ecf1b8d06c5d59451899586
Alistair Colling <alistair@Alistairs-MacBook-Pro.local>
2020-10-04 20:04:03 +0100

    ig

---
+++ README.md
@@ -0,0 +1,23 @@
+# Twitter API
+
+This is an express server deployed on AWS using Serverless
+
+## Still to do
+
+- Add Typescript
+- Tests
+
+# Dependency choices made
+
+- Serverless allowed me to quickly deploy to AWS
+- Twit package is a wrapper for the Twitter API
+
+## Running the Application Locally
+
+`npm i`
+`npm start`
+
+# Deployment
+
+`npm install -g serverless`
+`sls deploy`
--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
 const Twit = require("twit");
-let config = {
-  consumer_key: "hBMv81DL5k3kWAskc5H6jg",
-  consumer_secret: "MnQiXLobTXTSejYeScJPhM8e4uJy1Bg8mzgnw30BMA",
-  access_token: "16739856-IO5HvFzMRGNSrmNT0Nxp5GbXJvhQeZGVeT1XEqxIi",
-  access_token_secret: "rb2nYI6igVKAWX8yECndATiom1asRJKgQDt7MbBxggD72",
+const { prototype } = require("twit");
+const config = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
 };
 const Twitter = new Twit(config);
 const app = express();
@@ -16,21 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json("application/string"));
 app.use(cors());
 
-app.get("/api/info", (req, res) => {
-  res.send({ application: "sample-app", version: "1" });
+app.post("/api/v1/getUserTimeline", (req, res) => {
+  Twitter.get(
+    "statuses/user_timeline",
+    { screen_name: req.body.screen_name },
+    function (err, data, response) {
+      res.send({ tweets: data });
+    }
+  );
 });
-
-app.post("/api/v1/getback", (req, res) => {
-  console.log("request to API");
-  console.log(req.body);
-  //res.send("asdfasdfads");
-  Twitter.get("statuses/user_timeline", { screen_name: "alidrongo" }, function (
-    err,
-    data,
-    response
-  ) {
-    res.send({ tweets: data });
-  });
-});
-//app.listen(3001, () => console.log(`Listening on: 3001`));
 module.exports.handler = serverless(app);
