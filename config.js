4ca61b6eb655c7ea32457a2cb2262ac44216e0a6
Alistair Colling <alistair@Alistairs-MacBook-Pro.local>
2020-10-04 20:05:12 +0100

    ignore

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: "hBMv81DL5k3kWAskc5H6jg",
-  consumer_secret: "MnQiXLobTXTSejYeScJPhM8e4uJy1Bg8mzgnw30BMA",
-  access_token: "16739856-IO5HvFzMRGNSrmNT0Nxp5GbXJvhQeZGVeT1XEqxIi",
-  access_token_secret: "rb2nYI6igVKAWX8yECndATiom1asRJKgQDt7MbBxggD72",
-};
